c2b45ba9a5787e274cc25532a2399d29c8c7c047
1EEExp3rt <1EEExpErt@zju.edu.cn>
2024-12-23 22:49:43 +0800

    Updated spider: Suning

--- src/backend/utils/WebDriver.py
+++ src/backend/utils/WebDriver.py
@@ -5,17 +5,22 @@ The webdriver is Chrome.
 """
 
 import os
+from requests import options
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 
 driver_path = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "resources", "chromedriver")
 
-def get_webdriver() -> webdriver.Chrome:
+def get_webdriver(headless: bool = True) -> webdriver.Chrome:
     """
     Get a webdriver instance for spiders.
     """
 
+    options = Options()
+    if headless:
+        options.add_argument("--headless")
     return webdriver.Chrome(
-        options=webdriver.ChromeOptions(),
+        options=options,
         service=ChromeService(executable_path=driver_path)
     )
