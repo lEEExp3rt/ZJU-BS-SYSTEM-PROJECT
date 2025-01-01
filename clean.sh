c923d0523a5cbe1e06404f096143065a7efcbd12
1EEExp3rt <1EEExpErt@zju.edu.cn>
2025-01-01 15:58:31 +0800

    Changed some files

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-rm -rf bin/
+rm -rf build
 
 find . -name "__pycache__" -type d | xargs rm -rf
 find . -name ".pytest_cache" -type d | xargs rm -rf
+find . -name "Budget_Bee.egg-info" -type d | xargs rm -rf
